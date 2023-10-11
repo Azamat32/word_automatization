4276d502001b18ac1ad121f6bf37ec31f8548219
Azamat32 <azamat.chimchkov@gmail.com>
2023-10-11 19:59:09 +0600

    add optimazation

--- avtomotize1.py
+++ avtomotize1.py
@@ -1,130 +1,111 @@
 import os
 from docx import Document
 from openpyxl import load_workbook
-import psycopg2  # Import the library for working with PostgreSQL
+import psycopg2
 from docx.shared import Pt
 from docx.oxml.ns import nsdecls
 from docx.oxml import parse_xml
-from docx.enum.table import WD_ALIGN_VERTICAL
-from docx.shared import Pt, RGBColor, Inches
-from docx.oxml.ns import nsdecls
-from docx.oxml.ns import nsdecls
-from docx.oxml import parse_xml
 from docx.oxml.shared import OxmlElement
-from docx.oxml.ns import qn
-# Set up a connection to the PostgreSQL database
-conn = psycopg2.connect(
-    dbname="gdp_test",
-    user="postgres",
-    password="daniyarfgh",
-    host="localhost",
-    port="5432"
-)
+from docx.shared import RGBColor
 
-# Path to the directory where Excel files are stored
-tables_directory = "./tables_test/"
-
-# Path to the output document
-output_document_path = "./word/test1.docx"
-def set_cell_margins(cell, **kwargs):
-    tc = cell._tc
-    tcPr = tc.get_or_add_tcPr()
-    tcMar = OxmlElement('w:tcMar')
-
-    for m in ["top", "start", "bottom", "end"]:
-        if m in kwargs:
-            node = OxmlElement("w:{}".format(m))
-            node.set(qn('w:w'), str(kwargs.get(m)))
-            node.set(qn('w:type'), 'dxa')
-            tcMar.append(node)
-
-    tcPr.append(tcMar)
 def hex_to_rgb(hex_color):
     hex_color = hex_color.lstrip("#")
     return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
-# Create a new Word document
-new_document = Document()
 
-# Create a cursor for executing SQL queries
-cursor = conn.cursor()
+def set_text_color_to_white(cell):
+    for paragraph in cell.paragraphs:
+        for run in paragraph.runs:
+            run.font.color.rgb = RGBColor(255, 255, 255)
 
-# Get topics from the database
-cursor.execute("SELECT * FROM api_topic")
-topics = cursor.fetchall()
-
-# Iterate through topics
-for topic in topics:
-    topic_id, topic_name = topic
-    new_document.add_heading(topic_name, level=1)
-
-    # Get economic indexes for this topic
-    cursor.execute("SELECT * FROM api_economic_index WHERE macro_topic_id = %s", (topic_id,))
-    economic_indexes = cursor.fetchall()
-
-    # Iterate through economic indexes
-    for economic_index in economic_indexes:
-        economic_index_id, economic_index_name, *some = economic_index
-        new_document.add_heading(economic_index_name, level=2)
-
-        # Get table names for this economic index
-        cursor.execute("SELECT path FROM api_table WHERE macro_economic_index_id = %s", (economic_index_id,))
-        table_names = cursor.fetchall()
-
-        # Iterate through table names
-        for table_name in table_names:
-            table_name = table_name[0]  # Extract the table name from the tuple
-            excel_file_path = os.path.join(tables_directory, f"{table_name}.xlsx")
-            print(table_name)
-            try:
-                # Load the Excel file
-                excel_workbook = load_workbook(excel_file_path, data_only=True)
-                excel_sheet = excel_workbook.active
-                # Check if the Excel table is not empty
-                if excel_sheet.max_row > 0 and excel_sheet.max_column > 0:
-                    # Create a new table in the Word document
-                    num_rows = excel_sheet.max_row
-                    num_cols = excel_sheet.max_column
-                    table = new_document.add_table(rows=num_rows, cols=num_cols)
-                    table.style = 'Table Grid'
-                    # Apply cell formatting and add borders to paragraphs (use your provided code)
-                    for row in table.rows:
-                        for cell in row.cells:
-                            cell.paragraphs[0].style.font.size = Pt(10)
-                            for paragraph in cell.paragraphs:
+def process_table(new_document, table_name, tables_directory):
+    excel_file_path = os.path.join(tables_directory, f"{table_name}.xlsx")
+    print(table_name)
+    try:
+        # Load the Excel file
+        excel_workbook = load_workbook(excel_file_path, data_only=True)
+        excel_sheet = excel_workbook.active
+        if excel_sheet.max_row > 0 and excel_sheet.max_column > 0:
+            num_rows = excel_sheet.max_row
+            num_cols = excel_sheet.max_column
+            table = new_document.add_table(rows=num_rows, cols=num_cols)
+            table.style = 'Table Grid'
+            for row_idx, row in enumerate(table.rows):
+                for col_idx, cell in enumerate(row.cells):
+                    if row_idx == 0:
+                        # Set text color to white for the first row
+                        set_text_color_to_white(cell)
+                    shading_color = "#006FC0" if row_idx == 0 else "#fff"
+                    cell.paragraphs[0].style.font.size = Pt(8)
+                    shading_elm = parse_xml(f'<w:shd {nsdecls("w")} w:fill="{shading_color}" />')
+                    cell._tc.get_or_add_tcPr().append(shading_elm)
+            for row_idx, row in enumerate(excel_sheet.iter_rows()):
+                for col_idx, cell in enumerate(row):
+                    cell_value = cell.value if cell.value is not None else ""
+                    table.cell(row_idx, col_idx).text = str(cell_value)
+                    if row_idx == 0:
+            # Set text color to white for the first row
+                        for paragraph in table.rows[row_idx].cells[col_idx].paragraphs:
+                            if paragraph.runs:
                                 for run in paragraph.runs:
-                                    run.font.size = Pt(8)
-                                    
-                            shading_elm = parse_xml(r'<w:shd {} w:fill="ADD8E6"/>'.format(nsdecls('w')))
-                            cell._tc.get_or_add_tcPr().append(shading_elm)
+                                    run.font.color.rgb = RGBColor(255, 255, 255)
 
-                            
-                            # Add cell borders using the cell.borders property
-                            
+            new_document.add_paragraph()
+        else:
+            new_document.add_paragraph("Empty table: " + table_name)
+            new_document.add_paragraph()
+    except FileNotFoundError:
+        new_document.add_paragraph("Excel file not found: " + table_name)
+        new_document.add_paragraph()
+    except Exception as e:
+        print(f"Error processing table {table_name}: {str(e)}")
 
-                    # Fill the table with data from Excel
-                    for row_idx, row in enumerate(excel_sheet.iter_rows()):
-                        for col_idx, cell in enumerate(row):
-                            table.cell(row_idx, col_idx).text = str(cell.value)
+def main():
+    # Set up a connection to the PostgreSQL database
+    conn = psycopg2.connect(
+        dbname="gdp_test",
+        user="postgres",
+        password="daniyarfgh",
+        host="localhost",
+        port="5432"
+    )
 
-                    # Add an empty paragraph after the table for separation
-                    new_document.add_paragraph()
-                else:
-                    # If the Excel table is empty, add a corresponding message
-                    new_document.add_paragraph("Empty table: " + table_name)
-                    new_document.add_paragraph()  # Add an empty paragraph after the message
+    # Path to the directory where Excel files are stored
+    tables_directory = "./tables/"
 
-            except FileNotFoundError:
-                # If the Excel file is not found, add a corresponding message
-                new_document.add_paragraph("Excel file not found: " + table_name)
-                new_document.add_paragraph()  # Add an empty paragraph after the message
+    # Path to the output document
+    output_document_path = "./word/BulletTestTemplate.docx"
 
-            except Exception as e:
-                # Handle other exceptions if necessary
-                print(f"Error processing table {table_name}: {str(e)}")
+    # Create a new Word document
+    new_document = Document(output_document_path)
 
-# Save the Word document
-new_document.save(output_document_path)
+    # Create a cursor for executing SQL queries
+    cursor = conn.cursor()
 
-# Close the database connection
-cursor.close()
-conn.close()
+    # Fetch topics from the database
+    cursor.execute("SELECT * FROM api_topic")
+    topics = cursor.fetchall()
+
+    for topic in topics:
+        topic_id, topic_name = topic
+        new_document.add_heading(topic_name, level=1)
+
+        cursor.execute("SELECT * FROM api_economic_index WHERE macro_topic_id = %s", (topic_id,))
+        economic_indexes = cursor.fetchall()
+
+        for economic_index in economic_indexes:
+            economic_index_id, economic_index_name, *some = economic_index
+            new_document.add_heading(economic_index_name, level=2)
+
+            cursor.execute("SELECT path FROM api_table WHERE macro_economic_index_id = %s", (economic_index_id,))
+            table_names = cursor.fetchall()
+
+            for table_name in table_names:
+                process_table(new_document, table_name[0], tables_directory)
+
+    new_document.save("./word/test1.docx")
+
+    cursor.close()
+    conn.close()
+
+if __name__ == "__main__":
+    main()
